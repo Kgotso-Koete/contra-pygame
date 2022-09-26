86fc88b519280b86b05b50247119ed4e528846db
“Kgotso-Koete” <kgkoete@gmail.com>
2022-09-26 20:09:57 +0100

    bullets 1

--- bullet.py
+++ bullet.py
@@ -2,13 +2,19 @@ import pygame
 from settings import *
 from pygame.math import Vector2 as vector
 
-BULLET_SPEED = 200
+BULLET_SPEED = 1200
+MAX_LIFETIME = 1000
 
 
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, pos, surf, direction, groups):
         super().__init__(groups)
         self.image = surf
+
+        # flip the image if the direction.x is negative
+        if direction.x < 0:
+            self.image = pygame.transform.flip(self.image, True, False)
+
         self.rect = self.image.get_rect(center=pos)
         self.z = LAYERS["Level"]
 
@@ -17,6 +23,13 @@ class Bullet(pygame.sprite.Sprite):
         self.speed = BULLET_SPEED
         self.pos = vector(self.rect.center)
 
+        # destroy the bullet after some time
+        self.start_time = pygame.time.get_ticks()
+
     def update(self, dt):
         self.pos += self.direction * self.speed * dt
         self.rect.center = (round(self.pos.x), round(self.pos.y))
+
+        # destroy the bullet after some time
+        if (pygame.time.get_ticks() - self.start_time) > MAX_LIFETIME:
+            self.kill()
